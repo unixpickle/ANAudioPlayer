5448682f2cbee112b2abdb3ed764498dbb537fc2
Alex Nichol <alexnichol@comcast.net>
2011-12-07 19:09:15 -0500

    Initial Commit

---
+++ PlaySound/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PlaySound
+//
+//  Created by Alex Nichol on 12/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ PlaySound/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PlaySound
+//
+//  Created by Alex Nichol on 12/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
